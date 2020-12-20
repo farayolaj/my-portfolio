697527d6a9a91b8d4af08b249f062d4faf49cbc6
Joshua Farayola <farayolajoshua@gmail.com>
2020-12-20 17:31:06 +0100

    made svgs responsive in skill section

--- src/components/skills/SkillTemplate.tsx
+++ src/components/skills/SkillTemplate.tsx
@@ -1,14 +1,24 @@
-import { VStack } from '@chakra-ui/react';
+import { VStack, useBreakpointValue } from '@chakra-ui/react';
 import React from 'react';
 
 type SkillTemplateProp = {
   name: string,
   svg: JSX.Element
 }
+
 function SkillTemplate({ name, svg }: SkillTemplateProp): JSX.Element {
+  const size = useBreakpointValue({
+    base: 41,
+    md: 82
+  });
+
   return (
     <VStack>
-      {svg}
+      {React.cloneElement(svg, {
+        height: size,
+        width: size,
+        viewBox: '0 0 41 41'
+      })}
       <span>{name}</span>
     </VStack>
   );
