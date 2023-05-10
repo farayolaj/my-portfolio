fccfaa59899e37373d5e2109b2a2db905923e2c9
Joshua Farayola <farayolajoshua@gmail.com>
2023-05-10 17:44:27 +0100

    Updated eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,3 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 11,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+  extends: ['next/core-web-vitals', 'prettier'],
 };
