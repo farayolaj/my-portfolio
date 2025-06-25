332f6f46aa47f01abf4711283be56ff2f2dfc10c
Joshua Farayola <farayolajoshua@gmail.com>
2025-06-25 01:21:25 +0100

    fix: update Google Analytics tracking ID
    Replaced the Google Analytics tracking ID with the new one for accurate data collection.

--- src/components/lib/Analytics.tsx
+++ src/components/lib/Analytics.tsx
@@ -1,3 +1,4 @@
+//@ts-nocheck
 import Script from 'next/script';
 
 export default function Analytics() {
@@ -6,18 +7,17 @@ export default function Analytics() {
   return (
     <div>
       <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-0MDZSQ818D"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
+        src="https://www.googletagmanager.com/gtag/js?id=G-3RYY6V4M60"
+        onReady={() => {
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
+          function gtag() {
+            dataLayer.push(arguments);
+          }
           gtag('js', new Date());
 
-          gtag('config', 'G-0MDZSQ818D');
-        `}
-      </Script>
+          gtag('config', 'G-3RYY6V4M60');
+        }}
+      />
     </div>
   );
 }
